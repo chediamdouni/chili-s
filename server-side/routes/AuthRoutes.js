4225566f442ff4094c5f31675187a9cf616a3a3a
Chedi.Amdouni <chedi.amdouni@esprit.tn>
2023-12-04 00:05:46 +0100

    Login

--- server-side/routes/AuthRoutes.js
+++ server-side/routes/AuthRoutes.js
@@ -1,7 +1,9 @@
 var express = require("express");
-const {SignUp} = require("../controller/AuthController.js");
+const { SignUp, Login } = require("../controller/AuthController.js");
+const { userVerification } = require("../Middleware/AuthMiddleware.js");
 var router = express.Router();
 
 router.post("/signup", SignUp);
-
+router.post("/login", Login);
+router.post('/',userVerification)
 module.exports = router;
